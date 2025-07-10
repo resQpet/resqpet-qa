e79b43dcbf1d67afbbbee4afee1eb647d95f922e
Jordysvp <Jordysvp@gmail.com>
2025-07-10 17:17:32 -0400

    clean code

--- tests/login-register/logintest.spec.ts
+++ tests/login-register/logintest.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import 'dotenv/config';
 
+// Configuración global para la geolocalización simulada y el idioma
 test.use({
   permissions: ['geolocation'],
   geolocation: { latitude: 18.4861, longitude: -69.9312 }, 
@@ -8,33 +9,37 @@ test.use({
 });
 
 test.describe('Login', () => {
-  test('debe permitir iniciar sesión con credenciales válidas', async ({ page, context }) => {
-    
-    await page.goto('http://localhost:5173/auth/login');
 
+  // Test para login  con credenciales válidas
+  test('debe permitir iniciar sesión con credenciales válidas', async ({ page }) => {
+    await page.goto(`${process.env.BASE_URL}/auth/login`);
+
+    // Completar campos del formulario de inicio de sesión
     await page.getByRole('textbox', { name: 'Email' }).fill(process.env.EMAIL!);
     await page.getByRole('textbox', { name: 'Contraseña' }).fill(process.env.PASSWORD!);
 
-
+    // Enviar formulario
     await page.getByRole('button', { name: 'Iniciar sesión' }).click();
 
-    await expect(page.locator('#content')).toContainText('Publicaciones');
-    await expect(page).toHaveURL(/\/publications/);
+    // Validaciones de acceso
+    await expect(page).toHaveURL(/\/publications\/list/);
+
   });
 
-
-
-
+  // Test para login fallido con contraseña incorrecta
   test('debe mostrar mensaje de error si las credenciales son inválidas', async ({ page }) => {
+    await page.goto(`${process.env.BASE_URL}/auth/login`);
 
-    await page.goto('http://localhost:5173/auth/login');
-
+    // Completar campos con contraseña incorrecta
     await page.getByRole('textbox', { name: 'Email' }).fill(process.env.EMAIL!);
-    await page.getByRole('textbox', { name: 'Contraseña' }).fill('1234'); 
+    await page.getByRole('textbox', { name: 'Contraseña' }).fill('1234');
 
+    // Enviar formulario
     await page.getByRole('button', { name: 'Iniciar sesión' }).click();
 
+    // Verificar mensaje de error
     const errorMessage = await page.getByText('Usuario o contraseña incorrecto.');
     await expect(errorMessage).toBeVisible();
   });
+
 });
